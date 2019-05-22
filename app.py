9a58f1eee0797e9026b34cc09f0d9dffdebee372
yj7082126 <yk7da@virginia.edu>
2019-05-22 15:53:37 -0400

    new plan, switching to odbc

--- app.py
+++ app.py
@@ -1,38 +1,60 @@
 from azure.storage import CloudStorageAccount
-from flask import Flask, request, render_template, session, redirect
-from flask_table import Table, Col, ButtonCol
+from flask import Flask, request, render_template, redirect
+from flask_table import Table, Col
 import pandas as pd
+import pyodbc
+import struct
 app = Flask(__name__)
 
-account_name = 'pallystorageacc'
-account_key = 'BsM29pcnisLfDE2fe2PpOr3eU6cp6Bm4yKqHRvADhXZUZbNmYGlTo8HHwBmL6lGnnOolNa93n5Gsm/+6KAAy5w=='
-table_name = 'TodoItem'
-table_name2 = "Character"
+def handle_datetimeoffset(dto_value):
+    # ref: https://github.com/mkleehammer/pyodbc/issues/134#issuecomment-281739794
+    tup = struct.unpack("<6hI2h", dto_value)  # e.g., (2017, 3, 16, 10, 35, 18, 0, -6, 0)
+    tweaked = [tup[i] // 100 if i == 6 else tup[i] for i in range(len(tup))]
+    return "{:04d}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}.{:07d} {:+03d}:{:02d}".format(*tweaked)
+
+
+server = 'tcp:pallyserver.database.windows.net'
+database = 'PallyDB'
+username = 'ExternalPerson@pallyserver'
+password = 'S$*if9c lw8sdfdf'
+driver= '{ODBC Driver 13 for SQL Server}'
 
 class TaskTable(Table):
-    text = Col('Text')
-    update = Col('Updated')
-    complete = Col('Completed')
+    LU = Col('LU')
+    user = Col('user')
+    createAt = Col('createAt')
+    trial = Col('trial')
+    seconds = Col('seconds')
     
 class Task(object):
-    def __init__(self, text, update, complete):
-        self.text = text
-        self.update = update
-        self.complete = complete
+    def __init__(self, LU, user, createAt, trial, seconds):
+        self.LU = LU
+        self.user = user
+        self.createAt = createAt
+        self.trial = trial
+        self.seconds = seconds
         
 @app.route("/", methods=["GET", "POST"])
 def hello():
-    account = CloudStorageAccount(account_name, account_key)
-    table_service = account.create_table_service()
+    cnxn = pyodbc.connect("Driver=" + driver + ";Server=" + server 
+                      + ",1433;Database=" + database + 
+                      ";Uid=" + username + ";Pwd=" + password + ";")
+    cnxn.add_output_converter(-155, handle_datetimeoffset)
+    cursor = cnxn.cursor()
+    cursor.execute("SELECT * FROM [dbo].[LUItem]")
+    row = cursor.fetchall()
     
+    df = pd.DataFrame([list(t) for t in row], columns=["Id", "createAt", "updatedAt", 
+                                    "version", "deleted", "user", "trial",
+                                    "completed", "LU", "seconds"])
+    df2 = df[['LU', 'user', 'createAt', 'trial', 'seconds']]
+    df2['createAt'] = pd.to_datetime(df2['createAt'])
+    df2['createAt'] = df2['createAt'].apply(lambda x: x.strftime("%Y-%m-%d %H:%M:%S"))
+
     if request.method == 'POST':
         print(dict(request.form).keys[0])
     else:
-        df = []
-        tasks = table_service.query_entities(table_name, filter="PartitionKey eq 'tasks'")
-        for task in tasks:
-            df.append(Task(task.Text, task.Updated.strftime("%Y/%m/%d %H:%M:%S"), task.Completed))
-        table = TaskTable(df)
+        table = TaskTable(df2.to_dict(orient='records'))
         table.border = True
         
     return render_template('index.html', table = table.__html__())
